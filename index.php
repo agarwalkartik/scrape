f587b13c3781b2629f87bfa9c577aa8a45a608c3
kartikagarwal1993 <kartikagarwal.1993@gmail.com>
2014-11-13 13:39:56 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<title>Scraping Stack Overflow</title>
+<body>
+
+<h1>Select a tag you want to scrap</h1>
+
+<?php 
+include 'functions.php';
+showtags();//shows all the tags from database
+
+?>
+
+</body>
+</html>
